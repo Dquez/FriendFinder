269a93356fbffd467e264590c8db0d7a7318b479
Dariell Vasquez <dariell@Dariells-MacBook-Air.local>
2017-11-16 18:36:48 -0500

    Dynamically generated questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friends = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
         5,
         1,
         4,
@@ -12,11 +12,14 @@ let friends = [{
         5,
         4,
         1
-      ]
-  }];
+    ]
+}];
 
 
 
 
 
-  module.exports = friends;
+
+
+
+module.exports = friends;
