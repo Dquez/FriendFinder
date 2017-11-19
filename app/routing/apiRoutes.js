21035db291c3aa80cebbe8a90048c71b4c2f2395
Dariell Vasquez <dariell@Dariells-Air.fios-router.home>
2017-11-18 21:01:58 -0500

    Fixed post bug, used body-Parser after requiring routes instead of before

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,42 +6,10 @@ module.exports = function (app) {
 
     //Create new friend
     app.post("/api/friends", function (req, res) {
-
-        // //Empty array to push matched friend
-        // let bestFriend = {
-        //     name: "",
-        //     photo: "",
-        //     friendDifference: 1000
-        // };
-        // console.log(req.body);
         let userData = req.body;
-        // let userScores = userData.scores;
-        // let totalDifference = 0;
 
-        // //Loop through friends object and compare
-        // for (let i = 0; i < friendsData.length; i++) {
-
-        //     totalDifference = 0;
-
-        //     //Loop through the scores of each friend
-        //     for (let j = 0; j < friendsData[i].scores[j]; j++) {
-        //         //calculating the difference between each score and sum them into totalDifference
-        //         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
-
-        //         //Find best friend match
-        //         if (totalDifference <= bestFriend.friendDifference) {
-
-        //             bestFriend.name = friendsData[i].name;
-        //             bestFriend.photo = friendsData[i].photo;
-        //             bestFriend.friendDifference = totalDifference;
-
-        //         }
-        //     }
-        // }
         friendsData.push(userData);
         console.log(friendsData);
-
-        // res.json(bestFriend);
     });
 
 }
